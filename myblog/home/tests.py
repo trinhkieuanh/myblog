0f176decb8f7cd4e2b36e43e1f1eda7eaed77f27
trinhkieuanh <155094964+trinhkieuanh@users.noreply.github.com>
2024-08-12 13:09:06 +0700

    Add files via upload
    home

---
+++ myblog/home/tests.py
@@ -0,0 +1,5 @@
+from django.test import TestCase, SimpleTestCase
+class SimpleTests(SimpleTestCase):
+	def test_home_page_status(self):
+		response = self.client.get('/')
+		self.assertEquals(response.status_code, 200)
---
+++ myblog/home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from .form import RegistrationForm
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+# Create your views here.
+def index(request):
+    return render(request, 'pages/home.html')
+def contact(request):
+    return render(request, 'pages/contact.html')
+def error(request, exception=None):
+    return render(request, 'pages/error.html')
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
