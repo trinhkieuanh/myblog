19c82b04e96b2feadddc3b549496d610293e1f95
trinhkieuanh <155094964+trinhkieuanh@users.noreply.github.com>
2024-08-12 15:33:23 +0700

    Add files via upload

---
+++ myblog/blog/forms.py
@@ -0,0 +1,20 @@
+from typing import Any, Mapping
+from django import forms
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author', None)
+        self.post = kwargs.pop('post', None)
+        super().__init__(*args, **kwargs)
+    def save(self, commit=True):
+        comment = super().save(commit=False)
+        comment.author = self.author
+        comment.post = self.post
+        comment.save()
+    class Meta:
+        model = Comment
+        fields = ["body"]
---
+++ myblog/blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='blog'),
+    path('<int:pk>/', views.post, name='post'),
+]
